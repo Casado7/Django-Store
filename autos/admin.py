40f4815753b77a229f2d2585b95d70ebf783b807
Alejandro <alejandroacasadoc@gmail.com>
2024-03-06 16:25:57 -0400

    delete old apps, email settings, Orders App

--- autos/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Make, Auto
-# Register your models here.
-
-admin.site.register(Make)
-admin.site.register(Auto)
--- mysite/settings.py
+++ mysite/settings.py
@@ -136,6 +136,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "themasterale2@gmail.com"
+EMAIL_HOST_PASSWORD = "dtjmgqvxzmcoelps" #dtjm gqvx zmco elps 
 # Add the settings below
 
 REST_FRAMEWORK = {
